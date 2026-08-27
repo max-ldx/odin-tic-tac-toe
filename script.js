245c1d20a30ca08f921fd26cca8c1d9e4192a3c4
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-08-27 16:33:09 +0200

    Add board render

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const Tokens = Object.freeze({
+const token = Object.freeze({
     X: "X",
     O: "O"
 });
@@ -17,32 +17,35 @@ function createPlayer(name, token) {
 const gameboard = (() => {
     const board = [null, null, null, null, null, null, null, null, null];
 
-    const getBoard = () => [...board];
-
-    const tryPlaceToken = (token, index) => {
-        if (index < 0 || index > board.length - 1) return false;
-        if (board[index] !== null) return false;
+    const setTokenAtIndex = (token, index) => {
+        if (index < 0 || index > board.length - 1) return;
+        if (board[index] !== null) return;
 
         board[index] = token;
-        return true;
-    };
+    }
+
+    const getBoard = () => [...board];
 
     const isBoardFull = () => board.every(value => value !== null);
 
     const getBoardFillLevel = () => board.filter(value => value !== null).length;
 
     return {
+        setTokenAtIndex,
         getBoard,
-        tryPlaceToken,
         isBoardFull,
         getBoardFillLevel
     }
 })();
 
-const player1 = createPlayer("Player 1", Tokens.X);
-const player2 = createPlayer("Player 2", Tokens.O);
+const gameResult = Object.freeze({
+    None: 0,
+    Tie: 1,
+    Player1: 2,
+    Player2: 3
+})
 
-function createGameController(player1, player2, gameboard) {
+function createGameController(gameboard, player1, player2) {
     const winPositions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -52,17 +55,18 @@ function createGameController(player1, player2, gameboard) {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6]
-    ]
+    ];
 
-    const getCurrentPlayer = () => gameboard.getBoardFillLevel() % 2 === 0 ? player1 : player2;
+    const hasTie = () => gameboard.isBoardFull();
 
-    const checkHasWinner = () => {
+    const hasWinner = () => {
         const board = gameboard.getBoard();
 
         for (const winPosition of winPositions) {
             if (board[winPosition[0]] !== null &&
                 board[winPosition[0]] === board[winPosition[1]] &&
-                board[winPosition[1]] === board[winPosition[2]]) {
+                board[winPosition[1]] === board[winPosition[2]]
+            ) {
                 return true;
             }
         }
@@ -70,32 +74,48 @@ function createGameController(player1, player2, gameboard) {
         return false;
     }
 
-    const hasTie = () => gameboard.isBoardFull();
+    const getWinner = () => {
+        if (hasTie()) return gameResult.Tie;
+        if (!hasWinner()) return gameResult.None;
+        return (gameboard.getBoardFillLevel() - 1) % 2 === 0 ? gameResult.Player1 : gameResult.Player2;
+    }
 
     const playRound = (index) => {
-        if (checkHasWinner() || hasTie()) return;
-        const result = gameboard.tryPlaceToken(getCurrentPlayer().getToken(), index);
+        if (getWinner() !== gameResult.None) return;
+
+        const currentPlayer = gameboard.getBoardFillLevel() % 2 === 0 ? player1 : player2;
+        gameboard.setTokenAtIndex(currentPlayer.getToken(), index);
     }
 
     return {
-        checkHasWinner,
-        hasTie,
-        playRound
+        playRound,
+        getWinner
     }
 }
 
-const gameController = createGameController(player1, player2, gameboard);
+const player1 = createPlayer("player1", token.X);
+const player2 = createPlayer("player2", token.O);
+const gameController = createGameController(gameboard, player1, player2);
 
-gameController.playRound(0);
-console.log(gameboard.getBoard());
-gameController.playRound(3);
-console.log(gameboard.getBoard());
-gameController.playRound(1);
-console.log(gameboard.getBoard());
-gameController.playRound(4);
-console.log(gameboard.getBoard());
-gameController.playRound(2);
-console.log(gameboard.getBoard());
-gameController.playRound(5);
-console.log(gameboard.getBoard());
-console.log(gameController.checkHasWinner())
+const displayController = (() => {
+    const CELL_COUNT = gameboard.getBoard().length;
+    const boardContainer = document.querySelector(".board-container");
+
+    function renderBoard() {
+        boardContainer.textContent = null;
+        const board = gameboard.getBoard();
+
+        for (let i = 0; i < CELL_COUNT; i++) {
+            const button = document.createElement("button");
+            button.textContent = board[i];
+            button.addEventListener("click", () => {
+                gameController.playRound(i);
+                renderBoard();
+            });
+            boardContainer.appendChild(button);
+        }
+    }
+
+    // Render after players choose their name
+    renderBoard();
+})();
